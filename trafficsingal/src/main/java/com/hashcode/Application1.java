b5b53250019692ab6a479826634ab470bfb7e50e
wen.zhang <wen.zhang@klarna.com>
2021-02-25 20:13:04 +0100

    helper methods.

--- trafficsingal/src/main/java/com/hashcode/Application1.java
+++ trafficsingal/src/main/java/com/hashcode/Application1.java
@@ -1,4 +1,40 @@
 package com.hashcode;
 
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
 public class Application1 {
+
+    public Map<String, Street> streets;
+    public List<List<String>> carPaths;
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] getMetaInfo(InputStream is) {
+
+    }
+
+    private Map<String, Street> createStreets(InputStream is, int numberOfStreets) {
+
+    }
+
+    private List<List<String>> createCarPaths(InputStream is, int numberOfCars) {
+
+    }
+
+    private class Street {
+
+        private int start;
+        private int end;
+        private int time;
+
+        public Street(int start, int end, int time) {
+            this.start = start;
+            this.end = end;
+            this.time = time;
+        }
+    }
 }
